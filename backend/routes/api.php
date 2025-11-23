7dafd908c6142b0c7388f0907d28fdff3f2085cf
SPL3S <arnoldas.kukulskis@gmail.com>
2025-11-23 21:08:42 +0200

    Update api routes
    added booking and period routes for admin panel

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\WorkingHourController;
+use App\Http\Controllers\BreakPeriodController;
+use App\Http\Controllers\AdminBookingController;
 
 // Public routes
 Route::get('/services', [ServiceController::class, 'index']);
 Route::get('/available-slots', [BookingController::class, 'getAvailableSlots']);
+Route::get('/working-days', [BookingController::class, 'getWorkingDays']);
 Route::post('/bookings', [BookingController::class, 'store']);
 Route::get('/bookings', [BookingController::class, 'index']);
 
@@ -15,4 +18,15 @@ Route::get('/bookings', [BookingController::class, 'index']);
 Route::prefix('admin')->group(function () {
     Route::apiResource('services', ServiceController::class)->except(['index']);
     Route::apiResource('working-hours', WorkingHourController::class);
+
+    // Break periods routes
+    Route::get('/working-hours/{workingHour}/break-periods', [BreakPeriodController::class, 'index']);
+    Route::post('/working-hours/{workingHour}/break-periods', [BreakPeriodController::class, 'store']);
+    Route::put('/break-periods/{breakPeriod}', [BreakPeriodController::class, 'update']);
+    Route::delete('/break-periods/{breakPeriod}', [BreakPeriodController::class, 'destroy']);
+
+    // Bookings routes
+    Route::get('/bookings', [AdminBookingController::class, 'index']);
+    Route::put('/bookings/{id}', [AdminBookingController::class, 'update']);
+    Route::delete('/bookings/{id}', [AdminBookingController::class, 'destroy']);
 });
